17b40ff7568fcf4ef401224738075f1830aa8a3c
Viet Tran <vietnamtran@gmail.com>
2020-08-06 19:28:07 +0000

    First commit with some code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+# Configure server with Puppet
+package { 'nginx':
+    ensure   => installed,
+    provider => 'apt',
+}
+
+service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+    restart => 'sudo service nginx restart',
+}
+
+file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Holberton School',
+}
+
+file_line { '301 redirect':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    line   => return 301 'https':#www.holbertonschool.com$request_uri;
+    after  => '/var/www/html;'
+}
