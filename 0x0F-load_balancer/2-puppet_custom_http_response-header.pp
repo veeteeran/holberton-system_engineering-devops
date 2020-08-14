2624e5548bd7b5d39b39bbb7c1733fb9bc94e4b8
Viet Tran <vietnamtran@gmail.com>
2020-08-14 16:56:17 +0000

    Attempt 0

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+# Create custom HTTP header response using Puppet
+package { 'nginx':
+    ensure   => installed,
+    provider => 'apt',
+}
+
+service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+    restart => 'sudo service nginx restart',
+}
+
+file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Holberton School',
+}
+
+file_line { '301 redirect':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    line   => add_header X-Served-By $hostname;
+    after  => 'server_name _;'
+}
