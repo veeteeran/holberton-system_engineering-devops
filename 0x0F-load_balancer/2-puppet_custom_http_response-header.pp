aa6972dd14cca63e72ac31e657303d8fe1311325
Viet Tran <vietnamtran@gmail.com>
2020-08-15 01:47:28 +0000

    Change order of resources

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,12 +4,12 @@ package { 'nginx':
     provider => 'apt',
 }
 
+exec { 'Create custom HTTP header':
+    command => "/usr/bin/env sed -i '/^\tserver_name.*/a \\\tadd_header X-Served-By ${hostname};\n' /etc/nginx/sites-available/default"
+}
+
 service { 'nginx':
     ensure  => running,
     require => Package['nginx'],
     restart => 'sudo service nginx restart',
-}
-
-exec { 'Create custom HTTP header':
-    command => "/usr/bin/env sed -i '/^\tserver_name.*/a \\\tadd_header X-Served-By ${hostname};\n' /etc/nginx/sites-available/default"
 }
