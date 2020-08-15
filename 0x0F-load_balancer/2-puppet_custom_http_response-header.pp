b743c5822a1fa482ff3ee42c1a38a18353cfe849
Viet Tran <vietnamtran@gmail.com>
2020-08-15 01:39:19 +0000

    Use different path for bash delete update

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -10,12 +10,6 @@ service { 'nginx':
     restart => 'sudo service nginx restart',
 }
 
-exec { 'update':
-    path    => '/usr/bin/env',
-    command => 'apt-get update'
-}
-
 exec { 'Create custom HTTP header':
-    path    => '/usr/bin/env',
-    command => "sed -i '/^\tserver_name.*/a \\tadd_header X-Served-By ${hostname};\n' /etc/nginx/sites-available/default"
+    command => "/usr/bin/env bash sed -i '/^\tserver_name.*/a \\tadd_header X-Served-By ${hostname};\n' /etc/nginx/sites-available/default"
 }
