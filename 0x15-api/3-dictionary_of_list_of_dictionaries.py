6d2997926e1f133d7a4efeb25a32e11905b0ea2d
Viet Tran <vietnamtran@gmail.com>
2020-09-14 19:24:13 +0000

    First commit with tested code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" Building on task 0, export data in the CSV format"""
+from requests import get
+import json
+
+if __name__ == "__main__":
+    name_url = 'https://jsonplaceholder.typicode.com/users'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    names = get(name_url).json()
+    todos = get(todo_url)
+
+    output_dict = {}
+    write_list = []
+    wl_dict = {}
+    user = 1
+    for name in names:
+        for todo in todos.json():
+            if user == todo.get("userId"):
+                wl_dict.update({"task": todo.get("title")})
+                wl_dict.update({"completed": todo.get("completed")})
+                wl_dict.update({"username": name.get("username")})
+                write_list.append(wl_dict)
+                wl_dict = {}
+
+        output_dict.update({name.get("id"): write_list})
+        write_list = []
+        user += 1
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(output_dict, file)
