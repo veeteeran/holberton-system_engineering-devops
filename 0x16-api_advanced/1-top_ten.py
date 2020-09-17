a696129416a8a39b5347991207704248a60d591b
Viet Tran <vietnamtran@gmail.com>
2020-09-17 21:57:31 +0000

    Change docstring

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Returns a list containing the titles of all hot articles
-    for a given subreddit. Must use recursion
+""" prints the titles of the first 10 hot posts
+    listed for a given subreddit
 """
 from requests import get
 
