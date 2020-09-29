76698848a2c50d8b4e44e334659075826c358cd7
Viet Tran <vietnamtran@gmail.com>
2020-09-29 18:50:51 +0000

    Tested code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+# Using strace, find out why Apache is returning a 500 error
+exec { 'fix wordpress':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
