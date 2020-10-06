4b1bbcba9f6bced57e8009cf023e4f439f80e6c1
Viet Tran <vietnamtran@gmail.com>
2020-10-06 16:46:36 +0000

    script to fix nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+# Fix failed requests on nginx
+exec { 'fix--for-nginx':
+  command => "sed -i '5c ULIMIT=\"-n 4096\"' /etc/default/nginx && sudo service nginx restart",
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
