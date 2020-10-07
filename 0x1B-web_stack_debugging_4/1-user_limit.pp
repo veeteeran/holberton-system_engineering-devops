3c82a603c432f60f99b57c02775fb65363c4da43
Viet Tran <vietnamtran@gmail.com>
2020-10-07 17:05:29 +0000

    Tested code

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+# Change the OS configuration
+exec { 'Fix hard limit':
+  command => "sed -i '56c holberton hard nofile 1048576' /etc/security/limits.conf",
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+}
+
+exec { 'Fix soft limit':
+  command => "sed -i '57c holberton soft nofile 1048576' /etc/security/limits.conf",
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+}
